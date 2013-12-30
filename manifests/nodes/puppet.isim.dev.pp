5ecfa22911e7592d4fd4d2477deba1c97a8dc0bb
isim <ihcsim@gmail.com>
2013-12-29 23:14:41 -0800

    Add virtual host definitions in puppetmaster and puppet agent nodes.

--- manifests/nodes/puppet.isim.dev.pp
+++ manifests/nodes/puppet.isim.dev.pp
@@ -2,4 +2,13 @@ node 'puppet.isim.dev'{
   include base
   include postfix
   include mysql
+  include apache
+
+  apache::vhost{'stage.isim.example':
+    port          => 80,
+    docroot       => '/var/www/stage.isim.example',
+    ssl           => false,
+    priority      => 10,
+    serveraliases => 'home.stage.isim.example'
+  }
 }
--- manifests/nodes/puppetmaster.isim.dev.pp
+++ manifests/nodes/puppetmaster.isim.dev.pp
@@ -1,3 +1,12 @@
 node 'puppetmaster.isim.dev' {
   include base
+  include apache
+
+  apache::vhost{'dev.isim.example':
+    port          => 80,
+    docroot       => '/var/www/dev.isim.example',
+    ssl           => false,
+    priority      => 10,
+    serveraliases => 'dev.isim.example'
+  }
 }
